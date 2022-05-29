4607260b954f8f21320e3188d671dce3ab06793c
Ashen Lakshitha <ashen.lakshitha17879@gmail.com>
2022-05-29 17:35:57 +0530

    update some controlls

--- routes/subject.js
+++ routes/subject.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {
     getSubjects, 
     getSubject,
+    getSubjectPublic,
     getMySubjects,
     createSubject,
     updateSubject,
@@ -15,8 +16,10 @@ const { protect, authorize } = require('../middleware/auth');
 
 router
     .route('/')
-    .get(protect, getSubjects)
+    .get(getSubjects)
     .post(protect, authorize('teacher'), createSubject);
+
+    router.route('/public/:-..')
 
 router
     .route('/mysubjects')
@@ -26,11 +29,13 @@ router
     .route('/:subjectid')
     .get(protect, getSubject)
     .put(protect, authorize('teacher'), updateSubject)
-    .delete(protect, authorize('teacher'), deleteSubject);
+    .delete(protect, authorize('teacher'), deleteSubject);  
 
 router
     .route('/:subjectid/enroll')
     .put(protect, authorize('student'), enrollStudent);
+
+router.route('/public/:subjectid').get(getSubjectPublic)
 
     // router.post("/teachers/:teacherid/reviews", addReview);
 // router.put("/teachers/:teacherid/reviews/:reviewid", updateReview);
