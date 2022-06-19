8809010bf0b1b877d99c7d649e093bc6b6cf8578
Sandamali <sunethrasandamali0@gmail.com>
2022-06-19 10:20:38 +0530

    updated auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -176,6 +176,7 @@ const sendTokenResponse = (user, statusCode, res)=>{
             success: true, 
             token,
             role: user.role,
-            cart: user.cart.length
+            cart: user.cart.length,
+            id: user._id 
         });
 }
